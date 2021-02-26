3afdb98f0c2432acdb8f4990f90cfdf497c0b25b
Aeron01 <one.soulreaper@gmail.com>
2021-02-26 10:27:31 +0100

    update and corected

--- welcome.js
+++ welcome.js
@@ -1,6 +1,15 @@
 
 let firstname = "Bob"
 let gender = "male"
-let msg = firstname === 'Bob' && gender === 'male' || firstname === 'Alice' && gender === 'female' ? `Bonjour ${firstname}, vous êtes entrée sur le chan.` :
+let firstnameLc = firstname.toLocaleLowerCase()
+let genderLc = gender.toLowerCase()
+let msg = firstnameLc === 'bob' && genderLc === 'male' || firstnameLc === 'Alice' && genderLc === 'female' ? `Bonjour ${firstname}, vous êtes entrée sur le chan.` :
   `Bonjour ${firstname}, vous n'êtes pas autoriser à entrée sur le chan.`
 console.log(`${msg}`)
+
+
+// correction
+let firstname1 = "Bob"
+let gender1 = "female"
+
+console.log(`Bonjour ${firstname1}, vous etes entré${gender1 === "female" ? "e" : ""} sur le chan`)
